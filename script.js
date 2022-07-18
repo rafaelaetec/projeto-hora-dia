a6f16a4275d1b543f4b97ab5b92ca0523bd1b58c
Rafaela Moreira <rafaela.etecabdias@gmail.com>
2022-07-18 14:38:55 -0300

    Tornei o site responsivo

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ var data = new Date()
 var hora = data.getHours()
 var hora2 = data.getMinutes()
 if (hora >= 0 && hora < 12){
-    img.src = "../projeto-hora-dia/manha.jpg"
+    img.src = "manha.jpg"
     document.body.style.background = '#edc065'
     msg.innerHTML = `Bom dia! </br>Agora são<strong> ${hora}h${hora2}</strong>, tenha um ótimo dia!`
 }else if (hora >=12 && hora < 18){
-    img.src = "../projeto-hora-dia/tarde.jpg"
+    img.src = "tarde.jpg"
     document.body.style.background = '#fa8832'
     msg.innerHTML = `Boa tarde </br>
     Agora são <strong>${hora}h${hora2}</strong>, tenha uma ótima tarde!`
 }else{
-    img.src = "../projeto-hora-dia/noite.jpg"
+    img.src = "noite.jpg"
     document.body.style.background = '#0d1d50'
     msg.innerHTML = `Boa noite!</br>
     Agora são <strong>${hora}h${hora2}</strong>, tenha uma ótima noite!`
